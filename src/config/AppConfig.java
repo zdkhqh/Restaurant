e572b704d61a86c9c25b383812f17baec1dfdc1b
HuangQihan <huangqihan@tpson.cn>
2018-04-25 08:48:42 +0800

    RESTAURANT:201804250848

--- src/config/AppConfig.java
+++ src/config/AppConfig.java
@@ -14,29 +14,17 @@ import model._MappingKit;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * 本 demo 仅表达最为粗浅的 jfinal 用法，更为有价值的实用的企业级用法
- * 详见 JFinal 俱乐部: http://jfinal.com/club
- * <p>
- * API引导式配置
- */
+
 public class AppConfig extends JFinalConfig {
 
-    /**
-     * 运行此 main 方法可以启动项目，此main方法可以放置在任意的Class类定义中，不一定要放于此
-     * <p>
-     * 使用本方法启动过第一次以后，会在开发工具的 debug、run config 中自动生成
-     * 一条启动配置，可对该自动生成的配置再添加额外的配置项，例如 VM argument 可配置为：
-     * -XX:PermSize=64M -XX:MaxPermSize=256M
-     */
     public static void main(String[] args) {
         /**
-         * 特别注意：Eclipse 之下建议的启动方式
+         * 特别注意：Eclipse 之下的启动方式
          */
         //JFinal.start("WebRoot", 80, "/", 5);
 
         /**
-         * 特别注意：IDEA 之下建议的启动方式，仅比 eclipse 之下少了最后一个参数
+         * 特别注意：IDEA 之下的启动方式，仅比 eclipse 之下少了最后一个参数
          */
         JFinal.start("WebRoot", 80, "/");
     }
